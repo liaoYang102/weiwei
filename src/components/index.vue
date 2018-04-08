119986a83ecc049dc355f08523d728e1cf1b8bec
liaoYang102 <17775608995@163.com>
2018-04-08 17:22:13 +0800

    add index_router

---
+++ src/components/index.vue
@@ -0,0 +1,19 @@
+<template>
+	<h1>{{ msg }}</h1>
+</template>
+
+<script>
+export default {
+  name: 'index',
+  data () {
+    return {
+      msg: 'Welcome to Your Vue.js App'
+    }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style lang="less" scoped>
+
+</style>
