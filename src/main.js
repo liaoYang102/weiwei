4dd8dc957d87b3f0d8e78b01bb41c616127c0d44
liaoYang102 <17775608995@163.com>
2018-04-09 16:22:56 +0800

    add router

--- src/main.js
+++ src/main.js
@@ -8,7 +8,7 @@ import Mint from 'mint-ui'
 import Vuex from 'vuex'
 import echatrs from 'echarts'
 import axios from 'axios'
-import './assets/icons_font/iconfont.css';
+import './assets/icons_font/iconfont.css'
 import 'mint-ui/lib/style.css'
 
 Vue.use(Vuex)
@@ -16,7 +16,7 @@ Vue.use(Mint)
 
 Vue.config.productionTip = false
 
-const whiteList = ['/login', '/index', '/register'];// 不重定向白名单
+const whiteList = ['/login', '/index', '/register','/'];// 不重定向白名单
 router.beforeEach((to, from, next) => {
 
     if (store.getters.userstate) { // 
--- src/router/index.js
+++ src/router/index.js
@@ -2,6 +2,8 @@ import Vue from 'vue'
 import Router from 'vue-router'
 import index from '@/components/index'
 import navBar from '@/components/navBar'
+import login from '@/views/user/login'
+import reg from '@/views/user/reg'
 
 Vue.use(Router)
 
@@ -10,7 +12,17 @@ export default new Router({
     {
       path: '/',
       name: 'index',
-      component: navBar
+      component: index
+    },
+    {
+      path: '/user/login',
+      name: 'login',
+      component: login
+    },
+    {
+      path: '/user/reg',
+      name: 'login',
+      component; reg
     }
   ]
 })
