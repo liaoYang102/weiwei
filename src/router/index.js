610dfb1ad99b76bcc3bd3f3551d66bf9bb47de4b
liaoYang102 <17775608995@163.com>
2018-04-10 11:56:02 +0800

    add router

--- src/router/index.js
+++ src/router/index.js
@@ -4,7 +4,11 @@ import index from '@/components/index'
 import navBar from '@/components/navBar'
 import login from '@/views/user/login'
 import reg from '@/views/user/reg'
+import forget_pw from '@/views/user/forget_pw'
 import member from '@/views/member/index'
+import setting from '@/views/member/setting/index'
+import newphone from '@/views/member/setting/newphone'
+import oldphone from '@/views/member/setting/oldphone'
 import sorder from '@/views/sorder/index'
 
 Vue.use(Router)
@@ -30,10 +34,31 @@ export default new Router({
       path: '/member/index',
       name: 'member',
       component: member
-    },{
+    },
+    {
       path: '/sorder/index',
       name:'sorder',
       component: sorder
+    },
+    {
+      path: '/user/forget_pw',
+      name:'forget_pw',
+      component: forget_pw
+    },
+    {
+      path: '/member/setting/index',
+      name:'setting',
+      component: setting
+    },
+    {
+      path: '/member/setting/newphone',
+      name:'newphone',
+      component: newphone
+    },
+    {
+      path: '/member/setting/oldphone',
+      name:'oldphone',
+      component: oldphone
     }
   ]
 })
