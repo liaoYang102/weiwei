fff5fd01506102a0e628bd9dac91c873cdb0a534
user41 <user41@yulong.com>
2018-04-10 14:11:02 +0800

    complete setting/index.vue

---
+++ src/components/setting_header.vue
@@ -0,0 +1,17 @@
+<template>
+    <div>
+        <x-header :left-options="{backText: ''}" style="background:#1e6dfd;">{{ title }}</x-header>
+    </div>
+</template>
+
+<script>
+export default {
+    props: {
+        title: String  //定义传值的类型<br>    }
+    }
+}
+</script>
+
+
+
+
