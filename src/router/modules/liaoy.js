f55024e32085c43ab3d0e4f4f01a818b5f10a636
liaoYang102 <17775608995@163.com>
2018-04-12 15:40:53 +0800

    change router

---
+++ src/router/modules/liaoy.js
@@ -0,0 +1,11 @@
+import setting from '@/views/member/setting/index'
+
+const liaoy = [
+	{
+      path: '/member/setting/index',
+      name:'setting',
+      component: setting
+    }
+]
+
+export default liaoy
