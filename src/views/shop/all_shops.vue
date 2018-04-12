d5f34f007e55bf3899f906bfc4a499118e770ec7
user41 <user41@yulong.com>
2018-04-12 20:31:55 +0800

    add all_shops.vue code

--- src/views/shop/all_shops.vue
+++ src/views/shop/all_shops.vue
@@ -4,7 +4,7 @@
 
         <tab line-width='0' style="border-top: 1px solid #E1E1E1;">
 	        <tab-item selected @on-item-click="onItemClick">
-              综合 <span class="triangle"></span>
+              综合 <img src="../../assets/images/shop/xiaActive.png" alt="">
             </tab-item>
             <tab-item class='vux-center' @on-item-click="onItemClick">销量</tab-item>
             <tab-item @on-item-click="onItemClick">价格</tab-item>
@@ -14,19 +14,16 @@
             </tab-item>
 	    </tab>
 
-        <swiper :list="demo03_list" auto style="width:100%;margin:0 auto;" height="180px" dots-class="custom-bottom" dots-position="center"></swiper>
+        <swiper :list="demo03_list" auto style="width:100%;margin:0 auto;" height="2.2rem" dots-class="custom-bottom" dots-position="center"></swiper>
     
-        <div class="shopList">
-            <li></li>
-            <li></li>
-        </div>
-    
+        <shopList></shopList>
     
     </div>
 </template>
 
 <script>
 import settingHeader from '../../components/setting_header'
+import shopList from './components/all_shopsList'
 const imgList = [
   'http://placeholder.qiniudn.com/800x300/FF3B3B/ffffff',
   'http://placeholder.qiniudn.com/800x300/FFEF7D/ffffff',
@@ -39,7 +36,8 @@ const demoList = imgList.map((one, index) => ({
 }))
 export default {
     components: {
-        settingHeader
+        settingHeader,
+        shopList
     },
     data(){
         return {
@@ -49,19 +47,4 @@ export default {
     }
 }
 </script>
-
-<style lang="less" scoped>
-.triangle{
-    position: relative;
-    top: 0.2rem;
-    left: 0.1rem;
-    width: 0;
-    height: 0;
-    border-left: 0.07rem solid transparent;
-    border-right: 0.08rem solid transparent;
-    border-top: 0.1rem solid #336FFF;
-    border-radius: 0.02rem;
-}
-     
-</style>
 
---
+++ src/views/shop/components/all_shopsList.vue
@@ -0,0 +1,178 @@
+<template>
+    <div>
+        <scroller lock-x  height='-50' @on-scroll-bottom="onScrollBottom" ref="scrollerBottom" style="background-color:#efeff4" v-if='test'>
+            <div class="shopList">
+                <div class="all-shop">
+                    <div v-if="status == 1"> 
+                        <li v-for="(item,index) in list"  v-if="item.status ==1">
+                            <div class="list score">
+                                <div class="score-exchange">积分兑换</div>
+                                <div class="score-box">
+                                    <img src="../../../assets/images/shop/shop1.png" alt="">
+                                    <div class="text-box">
+                                        <span>{{ item.name }}</span>
+                                        <div class="score-num">{{ item.score}}</div>
+                                        <span class="remaining">{{ item.remaining}}</span>
+                                    </div>
+                                </div>
+                            </div>
+                        </li>
+                    </div>
+                    
+                    <div v-else-if="status == 2">
+                        <li v-for="(item,index) in list" v-if="item.status ==2">
+                            <div class="list">
+                                <div class="score-box">
+                                    <img src="../../../assets/images/shop/shop1.png" alt="">
+                                    <div class="text-box">
+                                        <span>{{ item.name }}</span>
+                                        <div>
+                                            <span class="money-red">{{ item.presentprice}}</span>
+                                            <div class="score-num" style="display:inline-block;">+{{ item.score}}</div>
+                                        </div>
+                                        <span class="remaining">
+                                        <span class="delete">{{ item.orprice}}</span>
+                                        <div class="right">{{ item.pin}}</div> 
+                                        </span>
+                                    </div>
+                                </div>
+                            </div>
+                        </li>
+                    </div>
+                    
+                </div>
+                
+
+                <load-more tip="loading" id='loading'></load-more>
+            </div>
+
+        </scroller>
+        
+    </div>
+</template>
+
+<script>
+export default {
+    data() {
+        return {
+            test: true,
+            list: [
+                { name: 'Vivo X21 屏幕指纹版全面...', score: '26554积分', remaining: '剩余24件', status: 1},
+                { name: 'Vivo X21 屏幕指纹版全面...', score: '26554积分', remaining: '剩余24件', status: 1},
+                { name: 'Vivo X21 屏幕指纹版全面...', presentprice: '￥3598' ,orprice: '￥4355', pin: '月销4714', score: '265积分', status: 2},
+                { name: 'Vivo X21 屏幕指纹版全面...', presentprice: '￥3598' ,orprice: '￥4355', pin: '月销4714', score: '265积分', status: 2}
+            ],
+            status: 2,
+            score: true,
+        }
+    },
+    methods:{
+        onScrollBottom(){
+	    	var load = document.getElementById("loading");
+	    	load.style.display = 'block'
+	    	if (this.onFetching) {
+		        // do nothing
+		      } else {
+		        this.onFetching = true
+		        setTimeout(() => {
+		        	load.style.display = 'none'
+		        	console.log(123);
+		          	this.$nextTick(() => {
+		            this.$refs.scrollerBottom.reset()
+		          })
+		          this.onFetching = false
+		        }, 2000)
+		      }
+	    }
+    }
+}
+</script>
+
+
+<style lang="less" scoped>
+.vux-loadmore{
+    display: none;
+}
+.shopList{
+    width: 100%;
+    background-color: #F5F6FA;
+    .all-shop{
+        li{
+            list-style: none;
+            float: left;
+            width: 50%;
+            height: 4.35rem;
+            font-size: 0.24rem;
+            color: #1A2642;
+            margin-bottom: 0.04rem;
+            .list{
+                margin-right:0.04rem;
+                background: #fff;
+                height: 4.35rem;
+                .score-exchange{
+                    position: absolute;
+                    left: -0.05rem;
+                    top: -0.05rem;
+                    width: 1.23rem;
+                    background-image: linear-gradient(241deg, #5EC3FF 1%, #106FE3 100%);
+                    text-align: center;
+                    color:#fff;
+                    font-size: 0.22rem;
+                    height: 0.36rem;
+                    line-height: 0.36rem;
+                    border-radius:0 0.05rem 0.05rem 0;
+                }
+                .score-box{
+                    img{
+                        display: block;
+                        margin: 0 auto;
+                        width: 78%;
+                    }
+                    .text-box{
+                        margin-left: 0.3rem;
+                        .money-red{
+                            font-size: 0.32rem;
+                            color: #F23030;
+                        }
+                        .score-num{
+                            width: 1.2rem;
+                            background-image: linear-gradient(241deg, #5EC3FF 1%, #106FE3 100%);
+                            text-align: center;
+                            color:#fff;
+                            font-size: 0.22rem;
+                            height: 0.36rem;
+                            line-height: 0.36rem;
+                            border-radius: 0.05rem;
+                            margin: 0.1rem 0 0.19rem 0;
+                            padding: 0.02rem 0.08rem;
+                        }
+                        .remaining{
+                            color: #90A2C7;
+                            .delete{
+                                text-decoration: line-through;
+                            }
+                            .right{
+                                float: right;
+                                margin-right: 0.28rem;
+                            }
+                        }
+                    }
+                }
+            }
+            .score{
+                height: 4.2rem;
+                position: relative;
+                border: 0.05rem solid;
+                border-image: linear-gradient(#5EC3FF, #106FE3) 30 30;
+            }
+            
+        }
+    }
+}
+</style>
+<style lang='less'>
+.vux-slider > .vux-swiper > .vux-swiper-item > a > .vux-swiper-desc{
+    background-image: none;
+}
+</style>
+
