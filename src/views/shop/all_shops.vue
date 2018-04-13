6355ee84603fc3a1bd77103f4466e908a8b4f8f5
user41 <user41@yulong.com>
2018-04-13 15:02:56 +0800

    modify all_shops.vue code

--- src/views/shop/all_shops.vue
+++ src/views/shop/all_shops.vue
@@ -1,40 +1,34 @@
 <template>
-    <div>
+    <div id="shops">
         <settingHeader :title="title"></settingHeader>
 
         <tab line-width='0' style="border-top: 1px solid #E1E1E1;">
-	        <tab-item selected @on-item-click="onItemClick">
-              综合 <img src="../../assets/images/shop/xiaActive.png" alt="">
+	        <tab-item selected @on-item-click="showPanel">
+              {{ tabItem}} <img src="../../assets/images/shop/xiaActive.png" alt="" width="6%">
             </tab-item>
             <tab-item class='vux-center' @on-item-click="onItemClick">销量</tab-item>
             <tab-item @on-item-click="onItemClick">价格</tab-item>
-            <tab-item @on-item-click="onItemClick">
+            <tab-item @on-item-click="onMenuClick">
                 筛选
             </tab-item>
 	    </tab>
 
-        <div class="panel">
-            <li>
-                <div class="selected">
-                    <span>综合</span> 
-                    <img src="../../assets/images/shop/selected.png" alt="">
+        <div v-transfer-dom>
+            <popup v-model="showMaskTop" position="top">
+                <div class="panel">
+                    <li v-for="(item, index) in panel"  @click="select(item,index)">
+                        <div :class="{'selected':act1==index}">
+                            <span>{{ item.title}}</span> 
+                            <img src="../../assets/images/shop/selected.png" alt="" v-if="act1 == index">
+                        </div>
+                    </li>
                 </div>
-            </li>
-            <li>
-                <div class="selected">
-                    <span>积分兑</span> 
-                    <img src="../../assets/images/shop/selected.png" alt="">
-                </div>
-            </li>
-            <li>
-                <div class="selected">
-                    <span>积分购</span> 
-                    <img src="../../assets/images/shop/selected.png" alt="">
-                </div>
-            </li>
+            </popup>
         </div>
 
-        <swiper :list="demo03_list" auto style="width:100%;margin:0 auto;" height="2.2rem" dots-class="custom-bottom" dots-position="center"></swiper>
+        <maskRight :value="value"></maskRight>
+
+        <swiper :list="demoList" auto style="width:100%;margin:0 auto;" height="2.2rem" dots-class="custom-bottom" dots-position="center"></swiper>
     
         <shopList></shopList>
     
@@ -44,6 +38,7 @@
 <script>
 import settingHeader from '../../components/setting_header'
 import shopList from './components/all_shopsList'
+import maskRight from './components/maskRight'
 const imgList = [
   'http://placeholder.qiniudn.com/800x300/FF3B3B/ffffff',
   'http://placeholder.qiniudn.com/800x300/FFEF7D/ffffff',
@@ -57,18 +52,67 @@ const demoList = imgList.map((one, index) => ({
 export default {
     components: {
         settingHeader,
-        shopList
+        shopList,
+        maskRight
     },
     data(){
         return {
             title: '手机',
-            demo03_list: demoList
+            tabItem: '综合',
+            demoList,
+            panel: [ 
+                { title: '综合', status: 0},
+                { title: '积分兑', status: 1}, 
+                { title: '积分购', status: 2}
+            ],
+            showMaskTop: false,
+            sMaskRight: false,
+            act1: 0,
+            value: false
         }
+    },
+    mounted:function(){
+        
+    },
+    methods: {
+        showPanel: function(){
+            if(this.showMaskTop == false){
+                this.showMaskTop = true;
+                // let hidden = document.getElementById('app')
+                // hidden.setAttribute("style", "overflow: hidden;");
+            }else {
+                this.showMaskTop = false
+            }
+            
+            
+        },
+        select: function(obj,i){
+            this.act1 = i;
+            this.showMaskTop = false;
+            this.tabItem = obj.title;
+            let hidden = document.getElementById('app')
+            hidden.setAttribute("style", "overflow: auto;");
+        },
+        confirmSelect: function(obj) {
+            
+        },
+        onMenuClick: function() {
+            this.showMaskTop = false;
+            this.value = true;
+            console.log('--', this.value)
+        }
+
     }
 }
 </script>
 
 <style lang="less" scoped>
+.vux-popup-dialog{
+    background: #fff;
+}
+.vux-popup-dialog.vux-popup-top{
+    top: 1.815rem;
+}
 .panel{
     width: 100%;
     height: 1.74rem;
@@ -85,6 +129,12 @@ export default {
            }
         }
     }
+}
+</style>
+
+<style lang="less">
+.vux-popup-mask{
+    top: 1.8rem !important;
 }
 </style>
 
---
+++ src/views/shop/components/maskRight.vue
@@ -0,0 +1,28 @@
+<template>
+	<div>
+		<div v-transfer-dom>
+		  <popup v-model="value" position="right">
+		    <div style="width:6.6rem;" class="screening">
+
+		    </div>
+		  </popup>
+		</div>
+	</div>
+</template>
+
+<script>
+export default {
+	props: {
+		value: Boolean
+	},
+    data(){
+    	return {
+    		showMaskRight: true,
+    	}
+    }
+}	
+</script>
+
+<style lang="less" scoped>
+	
+</style>
