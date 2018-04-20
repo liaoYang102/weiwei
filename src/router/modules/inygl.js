09476100d28f53a29872015af3b6b45b3ce327cb
Inygl <755508158@qq.com>
2018-04-20 17:10:17 +0800

    dd

--- src/router/modules/inygl.js
+++ src/router/modules/inygl.js
@@ -1,4 +1,6 @@
 import ShareIndex from '@/views/share/index'
+import Sdetail from '@/views/share/sdetail'
+import StoreList from '@/views/share/storelist'
 
 
 const inygl =[
@@ -6,6 +8,16 @@ const inygl =[
 		path:'/share',
 		name:'shareIndex',
 		component:ShareIndex
+	},
+	{
+		path:'/share/sdetail',//门店详情
+		name:'sdetail',
+		component:Sdetail
+	},
+	{
+		path:'/share/storelist',//门店列表
+		name:'storelist',
+		component:StoreList
 	}
 ]
 
