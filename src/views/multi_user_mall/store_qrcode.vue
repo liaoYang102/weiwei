3f77e33d90ca5e6f9c9fb2df122e7e5f0a980d42
user41 <user41@yulong.com>
2018-05-07 16:18:16 +0800

    add qrcode page

---
+++ src/views/multi_user_mall/store_qrcode.vue
@@ -0,0 +1,70 @@
+<template>
+	<section class="qrcode">
+		<settingHeader :title="title"></settingHeader>
+		<div class="blue">
+			<div class="content">
+				<div class="store">
+					<div class="name">店铺名</div>
+					<div>店铺分类</div>
+					<div class="mt">
+						<qrcode value="https://vux.li?x-page=demo_qrcode" :size="250" type="img"></qrcode>
+					</div>
+					<div class="btn">扫码二维码进入店铺</div>
+				</div>
+			</div>
+		</div>
+		<div class="clear"></div>
+	</section>
+</template>
+
+<script>
+	import settingHeader from '../../components/setting_header'
+	import { Qrcode } from 'vux'
+	export default {
+		components: {
+			settingHeader,Qrcode
+		},
+		data(){
+			return {
+				title: '我的店铺'
+			}
+		}
+	}
+</script>
+
+<style lang="less" scoped>
+	.qrcode{
+		background: #336FFF;
+		height: 100%;
+		.blue{
+			padding-top: 1.19rem;
+			.content{
+				width: 88.8%;
+				background: #fff;
+				margin: 0 auto;
+				padding-top: 0.72rem;
+				border-radius: 0.08rem;
+				.store{
+					width: 5.06rem;
+					margin: auto;
+					color: #1A2642;
+					font-size: 0.28rem;
+					padding-bottom: 0.91rem;
+					.name{
+						font-size: 0.48rem;
+						margin-bottom: 0.02rem;
+					}
+					.mt{
+						margin-top: 0.35rem;
+					}
+					.btn{
+						margin-top: 0.72rem;
+						font-size: 0.24rem;
+						color: #90A2C7;
+						text-align: center;
+					}
+				}
+			}
+		}
+	}
+</style>
