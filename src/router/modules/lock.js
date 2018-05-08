bf9fb668c39f418d10375430745197947d2f4c66
user41 <user41@yulong.com>
2018-05-08 20:33:03 +0800

    add router title

--- src/router/modules/lock.js
+++ src/router/modules/lock.js
@@ -5,10 +5,16 @@ const lock = [{
 	path: '/lock/card',
 	name: 'card',
 	component: card,
+	meta: {
+		title:'我的房卡'
+	}
 }, {
 	path: '/lock/list',
 	name: 'list',
 	component: list,
+	meta: {
+		title:'房卡列表'
+	}
 }, ]
 
 export default lock
--- src/router/modules/share.js
+++ src/router/modules/share.js
@@ -20,13 +20,17 @@ const share =[
 		// component:ShareIndex,
 		component:()=>import('@/views/share/index'),//异步组件
 		meta: {
-			navShow: true
+			navShow: true,
+			title: '共享设备'
 		}
 	},
 	{
 		path:'/share/sdetail',//门店详情
 		name:'sdetail',
 		component:Sdetail,
+		meta: {
+			title:'门店详情'
+		}
 		/*beforeEnter(to,from,next){
 			console.log(9);
 			next()
@@ -39,58 +43,89 @@ const share =[
 		props:true,
 		meta: {
 			navShow: true,
-			navIndex:3
+			navIndex:3,
+			title:'门店列表'
 		}
 	},
 	{
 		path:'/share/storelist2',//门店列表2
 		name:'storelist2',
-		component:StoreList2
+		component:StoreList2,
+		meta: {
+			title:'门店列表2'
+		}
 	},
 	{
 		path:'/share/meaidetail',//套餐详情
 		name:'meaidetail',
-		component:MeaiDetail
+		component:MeaiDetail,
+		meta: {
+			title:'套餐详情'
+		}
 	},
 	{
 		path:'/share/pintroduce',//威伐光介绍
 		name:'pintroduce',
-		component:PIntroduce
+		component:PIntroduce,
+		meta: {
+			title:'威伐光介绍'
+		}
 	},
 	{
 		path:'/share/join',//招商加盟
 		name:'join',
-		component:Join
+		component:Join,
+		meta: {
+			title:'招商加盟'
+		}
 	},
 	{
 		path:'/share/treatment',//治疗指导
 		name:'treatment',
-		component:TreatMent
+		component:TreatMent,
+		meta: {
+			title:'治疗指导'
+		}
 	},
 	{
 		path:'/share/comfirmOrder',//确认订单
 		name:'comfirmOrder',
-		component:ComfirmOrder
+		component:ComfirmOrder,
+		meta: {
+			title:'确认订单'
+		}
 	},
 	{
 		path:'/share/orderSuccess',//支付成功
 		name:'orderSuccess',
-		component:OrderSuccess
+		component:OrderSuccess,
+		meta: {
+			title:'支付成功'
+		}
 	},
 	{
 		path:'/share/orderDetail',//订单详情
 		name:'orderDetail',
-		component:OrderDetail
+		component:OrderDetail,
+		meta: {
+			title:'订单详情'
+		}
 	},
 	{
 		path:'/share/orderEval',//订单评价
 		name:'orderEval',
-		component:OrderEval
+		component:OrderEval,
+		meta: {
+			title:'订单评价'
+		}
 	},
 	{
 		path:'/share/usetime',//使用时间
 		name:'usetime',
-		component:UseTime
+		component:UseTime,
+		meta: {
+			title:'使用时间'
+		}
 	}
 ]
 
