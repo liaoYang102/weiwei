6f04c73a5ff2bc02ebf4badcd3c3d68594e905a1
1413224 <755508158@qq.com>
2018-05-16 16:25:00 +0800

    i

--- src/views/multi_user_mall/index.vue
+++ src/views/multi_user_mall/index.vue
@@ -1,82 +1,88 @@
 <template>
 	<section class="multi_user_mall">
-		<div class="wrapper" ref="wrapper">
-			<div class="content">
-				<settingHeader :title="title"></settingHeader>
-				<div class="top">
-					<div class="store">
-						<img src="../../assets/images/shop/UNIQLO.png">
-						<div class="name">
-							<div><span>优衣库官方旗舰店</span></div>
-							<div class="positionImg">
-								<!-- <img src="../../assets/images/multi_user_mall/position.png"> -->
-								<!-- <span class="small">距离正佳广场0.2km</span> -->
-								<img src="../../assets/images/multi_user_mall/right.png" class="rightImg">
-							</div>
-						</div>
-						<div class="btn" @click="focus" :class="{'btnActive': focusStatus == '已关注'}">{{focusStatus}}</div>
+		
+		<settingHeader :title="title"></settingHeader>
+		<div class="top">
+			<div class="store">
+				<img src="../../assets/images/shop/UNIQLO.png">
+				<div class="name">
+					<div><span>优衣库官方旗舰店</span></div>
+					<div class="positionImg">
+						<!-- <img src="../../assets/images/multi_user_mall/position.png"> -->
+						<!-- <span class="small">距离正佳广场0.2km</span> -->
+						<img src="../../assets/images/multi_user_mall/right.png" class="rightImg">
 					</div>
 				</div>
-				<div class="searchBox">
-					<div class="search">
-						<img src="../../assets/images/shop/search.png">
-						<input type="search" placeholder="搜索商品" @click="goSearch">
-					</div>
-				</div>
-
-				<div class="tab">
-					<tab :line-width="3" :scroll-threshold="4">
-						<tab-item selected @on-item-click="index">
-							<div class="tab-item">
-								<img src="../../assets/images/multi_user_mall/index1.png" v-if='pyy==1'>
-								<img src="../../assets/images/multi_user_mall/index.png" v-else><br>
-								<span>首页</span>
-							</div>
-						</tab-item>
-						<tab-item @on-item-click="onItemClick">
-							<div class="tab-item">
-								<img src="../../assets/images/multi_user_mall/shop1.png" v-if='pyy==2'>
-								<img src="../../assets/images/multi_user_mall/shop.png" v-else><br>
-								<span>商品</span>
-							</div>
-						</tab-item>
-						<tab-item @on-item-click="service">
-							<div class="tab-item">
-								<img src="../../assets/images/multi_user_mall/service1.png" v-if='pyy==3'>
-								<img src="../../assets/images/multi_user_mall/service.png" v-else><br>
-								<span>服务</span>
-							</div>
-						</tab-item>
-						<tab-item @on-item-click="store">
-							<div class="tab-item">
-								<img src="../../assets/images/multi_user_mall/store1.png" v-if='pyy==4'>
-								<img src="../../assets/images/multi_user_mall/store.png" v-else><br>
-								<span>简介</span>
-							</div>
-						</tab-item>
-					</tab>
-				</div>
-				<div class="index" v-if='pyy==1'>
-					<img src="../../assets/images/multi_user_mall/banner0.png" style="width: 100%">
-
-					<div class="imgList">
-						<div class="img" v-for="(item, index) in 3">
-							<img src="../../assets/images/multi_user_mall/1.png">
-						</div>
-					</div>
-
-					<img src="../../assets/images/multi_user_mall/Bitmap.png" style="width: 100%;">
-
-					<mallTheme :themeTitle="themeTitle"></mallTheme>
-					<mallTheme :themeTitle="themeTitle1"></mallTheme>
-				</div>
-				<shop v-else-if="pyy==2"></shop>
-				<service v-else-if="pyy==3"></service>
-				<store v-else-if="pyy==4"></store>
-				<loading v-if="show && pyy!=3 && pyy!=4"></loading>
-				<noMore v-if="showNomore && pyy!=3 && pyy!=4"></noMore>
+				<div class="btn" @click="focus" :class="{'btnActive': focusStatus == '已关注'}">{{focusStatus}}</div>
 			</div>
 		</div>
+		<div class="searchBox">
+			<div class="search">
+				<img src="../../assets/images/shop/search.png">
+				<input type="search" placeholder="搜索商品" @click="goSearch">
+			</div>
+		</div>
+
+		<div class="mallTab">
+			<tab :line-width="3" :scroll-threshold="4">
+				<tab-item selected @on-item-click="index">
+					<div class="tab-item">
+						<img src="../../assets/images/multi_user_mall/index1.png" v-if='pyy==1'>
+						<img src="../../assets/images/multi_user_mall/index.png" v-else><br>
+						<span>首页</span>
+					</div>
+				</tab-item>
+				<tab-item @on-item-click="onItemClick">
+					<div class="tab-item">
+						<img src="../../assets/images/multi_user_mall/shop1.png" v-if='pyy==2'>
+						<img src="../../assets/images/multi_user_mall/shop.png" v-else><br>
+						<span>商品</span>
+					</div>
+				</tab-item>
+				<tab-item @on-item-click="service">
+					<div class="tab-item">
+						<img src="../../assets/images/multi_user_mall/service1.png" v-if='pyy==3'>
+						<img src="../../assets/images/multi_user_mall/service.png" v-else><br>
+						<span>服务</span>
+					</div>
+				</tab-item>
+				<tab-item @on-item-click="store">
+					<div class="tab-item">
+						<img src="../../assets/images/multi_user_mall/store1.png" v-if='pyy==4'>
+						<img src="../../assets/images/multi_user_mall/store.png" v-else><br>
+						<span>简介</span>
+					</div>
+				</tab-item>
+			</tab>
+		</div>
+		<div style="height: 67.2%"  v-if="pyy!=2">
+			<div class="wrapper" ref="wrapper">
+				<div class="content">
+					<div class="index" v-if='pyy==1'>
+						<img src="../../assets/images/multi_user_mall/banner0.png" style="width: 100%">
+
+						<div class="imgList">
+							<div class="img" v-for="(item, index) in 3">
+								<img src="../../assets/images/multi_user_mall/1.png">
+							</div>
+						</div>
+
+						<img src="../../assets/images/multi_user_mall/Bitmap.png" style="width: 100%;">
+
+						<mallTheme :themeTitle="themeTitle"></mallTheme>
+						<mallTheme :themeTitle="themeTitle1"></mallTheme>
+					</div>
+					
+					<service v-else-if="pyy==3"></service>
+					<store v-else-if="pyy==4"></store>
+					<loading v-if="show && pyy!=3 && pyy!=4 && pyy!= 1"></loading>
+					<noMore v-if="showNomore && pyy!=3 && pyy!=4"></noMore>
+				</div>
+			</div>
+		</div>
+		
+
+		<div style="height: 65.5%" v-else><shop style="height: 100%"></shop></div>
 	</section>
 </template>
 
@@ -114,17 +120,31 @@
 			this.InitScroll()
 		},
 		methods: {
+			changeScroll(){
+				this.$nextTick(() => {
+					this.scroll = new BScroll(this.$refs.wrapper, {
+						click: true,
+						scrollY: true,
+						pullUpLoad: {
+							threshold: -30, // 负值是当上拉到超过低部 70px；正值是距离底部距离 时，                    
+						}
+					})
+				})
+			},
 			onItemClick() {
 				this.pyy = 2;
 			},
 			index() {
 				this.pyy = 1;
+				this.changeScroll()
 			},
 			service() {
 				this.pyy = 3;
+				this.changeScroll()
 			},
 			store() {
 				this.pyy = 4;
+				this.changeScroll()
 			},
 			goSearch() {
 				this.$router.push({
@@ -251,11 +271,14 @@
 		}
 	}
 	
-	.tab {
+	.mallTab {
 		height: 1.2rem;
 		.tab-item {
 			padding-top: 0.19rem;
-			/*padding-bottom: 0.2rem;*/
+			img {
+				width: 20%;
+				display: inline-block;
+			}
 		}
 	}
 	
@@ -317,29 +340,26 @@
 </style>
 
 <style lang="less">
-	.multi_user_mall .tab .vux-tab-warp {
+	.multi_user_mall .mallTab .vux-tab-container {
+		height: 1.2rem;
+	}
+	
+	.multi_user_mall .mallTab .vux-tab {
+		height: 1.2rem;
+	}
+	.multi_user_mall .mallTab .vux-tab-warp {
 		padding-top: 1.2rem;
 	}
 	
-	.multi_user_mall .tab .vux-tab .vux-tab-item {
+	.multi_user_mall .mallTab .vux-tab .vux-tab-item {
 		color: #1A2642;
 		font-size: 0.24rem;
 		line-height: normal;
-		img {
-			width: 20%;
-			display: inline-block;
-		}
+		border:none;
 	}
 	
-	.multi_user_mall .tab .vux-tab-selected {
+	.multi_user_mall .mallTab .vux-tab .vux-tab-selected {
 		color: #336FFF !important;
 	}
 	
-	.multi_user_mall .tab .vux-tab-container {
-		height: 1.2rem !important;
-	}
-	
-	.multi_user_mall .tab .vux-tab {
-		height: 1.2rem !important;
-	}
 </style>
